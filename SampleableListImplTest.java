ebd5f7186a317b69dba1138d3c2b06330b036ff3
fbarto01 <federico.bartolomei3@studio.unibo.it>
2015-01-03 06:05:41 +0000

    added SampleableListImplTest

---
+++ SampleableListImplTest.java
@@ -0,0 +1,35 @@
+
+public class SampleableListImplTest {
+	
+	public static void main(String[] args) {
+		TestList t = new TestList();
+		List a = new ArrayList();
+		System.out.println("=============================================");
+		System.out.println("new ArrayList() \"a\" created, with no elements");
+		t.test1(a);
+		SampleableList s = new SampleableListImpl(a);
+		System.out.println("new SampleableList() \"s\" created from the previous ArrayList \"a\":");
+		System.out.println("test1(s.sample()) - should return the same empty list");
+		t.test1(s.sample());
+		List l = new LinkedList();
+		l.add(0);
+		l.add("String at index 1 - should be discarded");
+		l.add("String at index 2 - should be returned as index 1");
+		l.add("String at index 3 - should be discarded");
+		l.add("String at index 4 - should be returned as index 2");
+		l.add("String at index 6 - should be returned as index 3");
+		l.add(5, false);
+		System.out.println("new LinkedList() created:");
+		t.printList(l);
+		s = new SampleableListImpl(l);
+		System.out.println("new SampleableList() created on previous LinkedList (should be an exact copy):");
+		t.printList(s);
+		System.out.println("testing sample():");
+		t.printList(s.sample());
+
+	}
+	
+}
+		
+		
+		
