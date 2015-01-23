4742eff68b96f8c1f07b8083f1955b50cf5fdc86
fbarto01 <federico.bartolomei3@studio.unibo.it>
2015-01-23 07:32:30 +0000

    added javadoc

--- SampleableListImpl.java
+++ SampleableListImpl.java
@@ -1,9 +1,21 @@
-
+/**
+ * Implementation of interface {@see SampleableList}
+ * All the methods from interface {@list} are overridden,
+ * using the implementations of the underlying list provided
+ * with the constructor. 
+ *
+ * @author federico bartolomei (bbk-pij-2014-21)
+ */
 public class SampleableListImpl implements SampleableList {
 	private List list;
-	
-	public SampleableListImpl(List l) {
-		list = l;
+    /**
+     * Create a new SampleableListImpl with the provided list
+     * as the underlying data structure.
+     * 
+     * @param list the list underlying this implementation
+     */
+	public SampleableListImpl(List list) {
+		this.list = list;
 	}
 	
 	public SampleableList sample() {
@@ -13,32 +25,50 @@ public class SampleableListImpl implements SampleableList {
 		}
 		return s;
 	}
-	
-	@Override
+    /**
+     * Method from interface {@see List} calls the implementation
+     * provided by the list set up with the constructor
+     */
+    @Override
 	public boolean isEmpty() {
 		return list.isEmpty();
 	}
-	
-	@Override
+    /**
+     * Method from interface {@see List} calls the implementation
+     * provided by the list set up with the constructor
+     */
+    @Override
 	public int size() {
 		return list.size();
 	}
-	
-	@Override
+    /**
+     * Method from interface {@see List} calls the implementation
+     * provided by the list set up with the constructor
+     */
+    @Override
 	public ReturnObject get(int index) {
 		return list.get(index);
 	}
-	
-	@Override
+    /**
+     * Method from interface {@see List} calls the implementation
+     * provided by the list set up with the constructor
+     */
+    @Override
 	public ReturnObject remove(int index) {
 		return list.remove(index);
 	}
-	
-	@Override
+    /**
+     * Method from interface {@see List} calls the implementation
+     * provided by the list set up with the constructor
+     */
+    @Override
 	public ReturnObject add(int index, Object item) {
 		return list.add(index, item);
 	}
-	
+    /**
+     * Method from interface {@see List} calls the implementation
+     * provided by the list set up with the constructor
+     */
 	@Override
 	public ReturnObject add(Object item) {
 		return list.add(item);
